83a8b876a91c67b69b884f391c16bbe0715397cc
Timo Mämecke <timo@maemecke.com>
2025-05-10 16:37:23 +0200

    Add lib facade comment to slugify

--- src/lib/slugify.ts
+++ src/lib/slugify.ts
@@ -1,5 +1,6 @@
 import sindreSlugify from '@sindresorhus/slugify'
 
+/** Turn any string into a slug. Smol lib facade around sindre's slugify */
 export function slugify(str: string) {
   return sindreSlugify(str)
 }
